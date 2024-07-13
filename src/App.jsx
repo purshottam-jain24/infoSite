b5439fcd23dbf835c7011e8733bd3e64d9ba0a76
purshottam-jain24 <purshotamjaindelhi11@gmail.com>
2024-07-13 14:27:53 +0530

    [Initial Commit]

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import React from "react"
+import Navbar from "./assets/components/navbar"
+import Main from "./assets/components/body"
+import "./App.css"
+
+export default function App() {
+    const [darkMode, setDarkMode] = React.useState(true)
+    
+    function toggleDarkMode() {
+        setDarkMode(prevMode => !prevMode)
+    }
+    
+    return (
+        <div className="container">
+            <Navbar 
+                darkMode={darkMode} 
+                toggleDarkMode={toggleDarkMode}
+            />
+            <Main darkMode={darkMode} />
+        </div>
+    )
+}
